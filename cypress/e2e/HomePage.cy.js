e8a024d5ab183c6428a41a227f2d5a6cb4c51be0
Alex Perez-Davies <alexperezdavies@gmail.com>
2022-12-13 13:43:10 +0000

    Added homepage tests

--- cypress/e2e/HomePage.cy.js
+++ cypress/e2e/HomePage.cy.js
@@ -1,6 +1,27 @@
-describe("My First Test", () => {
-  it("Visits homepage", () => {
+beforeEach(() => {
+  cy.visit("http://localhost:3000");
+});
+describe("Check elements", () => {
+  it(`Visits homepage`, () => {
     cy.visit("http://localhost:3000");
-    cy.click(button);
+  });
+  it(`Check for h1`, () => {
+    cy.contains("Citizen Ticket Challenge");
+  });
+  it(`Has input`, () => {
+    cy.contains("category");
+  });
+  it(`Has choose colour button`, () => {
+    cy.get("button").should("have.class", "choose-colour");
+  });
+  it(`Has choose icon button`, () => {
+    cy.get("button").should("have.class", "choose-icon");
+  });
+  it(`Has create category button`, () => {
+    cy.contains("Create Category");
+  });
+  it(`Has a next link that goes to category page`, () => {
+    cy.contains("Next").click();
+    cy.url().should("include", "category");
   });
 });
